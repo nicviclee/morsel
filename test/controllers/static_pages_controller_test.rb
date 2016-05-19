a8025009395d50f16df2da909a951671e5c05d4d
nicviclee <nicviclee@hotmail.com>
2016-05-19 12:56:20 -0700

    Factor out redundancy in tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,15 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+  	@base_title = "Morsel"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Morsel"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | Morsel"
+  	assert_select "title", "About | #{@base_title}"
   end
 end
