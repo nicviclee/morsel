dec38c339a0356b2c14bca1eef0624f1c25d3b4c
nicviclee <nicviclee@hotmail.com>
2016-05-21 23:39:53 -0700

    Add image upload option for microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522003756) do
+ActiveRecord::Schema.define(version: 20160522061127) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160522003756) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
